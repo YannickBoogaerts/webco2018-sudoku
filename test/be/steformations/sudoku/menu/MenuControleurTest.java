f198e629eeb8ebf153320d0f090973918deeca0b
yannick <yannick.boogaerts@ulg.ac.be>
2018-02-26 16:44:58 +0100

    Etape 8.1
    Il est possible de modifier et de récupérer la vue d'un controleur.

--- test/be/steformations/sudoku/menu/MenuControleurTest.java
+++ test/be/steformations/sudoku/menu/MenuControleurTest.java
@@ -10,12 +10,24 @@ public class MenuControleurTest {
 	 * test : Il est possible de modifier et de récupérer le menu d'un controleur.
 	 */
 	@Test
-	public void testVueMenuProperty() {
+	public void testMenuProperty() {
 		MenuControleur controleur = new  MenuControleur();
 		Menu menu = new Menu();
 		controleur.setMenu(menu);
 		Menu menu2 = controleur.getMenu();
 		assertSame(menu, menu2);
+	}
+	
+	/**
+	 * test : Il est possible de modifier et de récupérer la vue d'un controleur.
+	 */
+	@Test
+	public void testVueProperty() {
+		MenuControleur controleur = new  MenuControleur();
+		VueMenu vue = new VueMenu();
+		controleur.setVue(vue);
+		VueMenu vue2 = controleur.getVue();
+		assertSame(vue, vue2);
 	}
 
 }
